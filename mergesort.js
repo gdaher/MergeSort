6af55816adb992d23324d3702f4f0a293ae80262
Jolene Langlinais <jolenelanglinais@gmail.com>
2018-09-17 15:48:26 -0400

    mergeArr three specs

--- mergesort.js
+++ mergesort.js
@@ -8,5 +8,28 @@ function split(arr) {
     let arr1 = arr.slice(0,midIdx);
     let arr2 = arr.slice(midIdx);
 
-    return [arr1, arr2];
+    if (!oddRemainder) {
+      return [arr1, arr2];
+    } else {
+      return [arr1, arr2, oddRemainder];
+    }
+}
+
+function mergeArr (arrInput1, arrInput2) {
+  let resultArr = [];
+  while (arrInput1.length > 0 && arrInput2.length > 0) {
+    if (arrInput1[0] < arrInput2[0]) {
+      resultArr.push(arrInput1.shift());
+    } else if (arrInput1[0] === arrInput2[0]) {
+      resultArr.push(arrInput1.shift(), arrInput2.shift());
+    } else {
+      resultArr.push(arrInput2.shift());
+    }
+  }
+  if (arrInput1.length > 0) {
+    resultArr = resultArr.concat(arrInput1);
+  } else if (arrInput2.length > 0) {
+    resultArr = resultArr.concat(arrInput2);
+  }
+  return resultArr;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -3,6 +3,13 @@ describe('Split Array function', function() {
         expect(split([1,2,3,4,5,6,7,8])).toEqual([[1,2,3,4],[5,6,7,8]]);
     });
     it('is able to split an odd array into two halves', function() {
-        expect(split([1,2,3,4,5,6,7,8,9])).toEqual([[1,2,3,4],[5,6,7,8]]);
+        expect(split([1,2,3,4,5,6,7,8,9])).toEqual([[1,2,3,4],[5,6,7,8],9]);
     });
 });
+
+
+describe('Merge function', function(){
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      expect(mergeArr([1,5,9], [0,3,7])).toEqual([0,1,3,5,7,9]);
+    });
+  });
